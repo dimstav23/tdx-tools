ecfefa1315470e608f102bcf338503f3eba5b0dd
Dimitrios Stavrakakis <dimitrios.stavrakakis@intel.com>
2023-10-02 15:29:54 -0700

    add scripts for the native versions of the benchmark variants

---
+++ benchmarks/pytorch/native_pytorch_benchmark_runner.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/bash
+
+set -e
+
+THIS_DIR=$(dirname "$(readlink -f "$0")")
+DEPS_DIR=$THIS_DIR/deps
+RESULTS_DIR=$THIS_DIR/results
+GRAMINE_SGX_INSTALL_DIR=$DEPS_DIR/gramine/build-release
+GRAMINE_TDX_INSTALL_DIR=$DEPS_DIR/dkuvaisk.gramine-tdx/build-release
+BENCHMARK_DIR=$DEPS_DIR/examples/pytorch
+VM_MEM=16G
+
+THREADS=(1 2 4 6 8 12 16 20 24 32)
+
+pushd $BENCHMARK_DIR
+
+#create temp directory for the results
+mkdir -p results
+
+# Run the native case
+for THREAD_CNT in "${THREADS[@]}"; do
+  export OMP_NUM_THREADS=$THREAD_CNT
+  python3 pytorchexample.py $THREAD_CNT native
+done
+
+# Preserve the current values of the env variables
+CURR_PATH=$PATH
+CURR_PYTHONPATH=$PYTHONPATH
+CURR_PKG_CONFIG_PATH=$PKG_CONFIG_PATH
+
+# Run the bare-metal (bm) gramine-direct and gramine-sgx case
+export PATH=$GRAMINE_SGX_INSTALL_DIR/bin:$CURR_PATH
+export PYTHONPATH=$GRAMINE_SGX_INSTALL_DIR/lib/python3.10/site-packages:$CURR_PYTHONPATH
+export PKG_CONFIG_PATH=$GRAMINE_SGX_INSTALL_DIR/lib/x86_64-linux-gnu/pkgconfig:$CURR_PKG_CONFIG_PATH
+make clean && make SGX=1
+for THREAD_CNT in "${THREADS[@]}"; do
+  export OMP_NUM_THREADS=$THREAD_CNT
+  gramine-direct ./pytorch pytorchexample.py $THREAD_CNT bm-gramine-direct
+done
+for THREAD_CNT in "${THREADS[@]}"; do
+  export OMP_NUM_THREADS=$THREAD_CNT
+  gramine-sgx ./pytorch pytorchexample.py $THREAD_CNT bm-gramine-sgx
+done
+
+# Run the gramine-vm and gramine-tdx case
+export PATH=$GRAMINE_TDX_INSTALL_DIR/bin:$CURR_PATH
+export PYTHONPATH=$GRAMINE_TDX_INSTALL_DIR/lib/python3.10/site-packages:$CURR_PYTHONPATH
+export PKG_CONFIG_PATH=$GRAMINE_TDX_INSTALL_DIR/lib/x86_64-linux-gnu/pkgconfig:$CURR_PKG_CONFIG_PATH
+make clean && make SGX=1
+for THREAD_CNT in "${THREADS[@]}"; do
+  export QEMU_CPU_NUM=$THREAD_CNT
+  export QEMU_MEM_SIZE=$VM_MEM
+  export OMP_NUM_THREADS=$THREAD_CNT
+  gramine-vm ./pytorch pytorchexample.py $THREAD_CNT gramine-vm
+done
+for THREAD_CNT in "${THREADS[@]}"; do
+  export QEMU_CPU_NUM=$THREAD_CNT
+  export QEMU_MEM_SIZE=$VM_MEM
+  export OMP_NUM_THREADS=$THREAD_CNT
+  gramine-tdx ./pytorch pytorchexample.py $THREAD_CNT gramine-tdx
+done
+
+mkdir -p $RESULTS_DIR
+mv results/* $RESULTS_DIR
+rm -rf results
+
+popd
---
+++ benchmarks/pytorch/native_pytorch_benchmark_setup.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/bash
+
+set -e
+
+THIS_DIR=$(dirname "$(readlink -f "$0")")
+DEPS_DIR=$THIS_DIR/deps
+EXAMPLES_PATCH=$THIS_DIR/gramine_examples.patch
+
+# Create the directory for the dependencies
+mkdir -p $DEPS_DIR
+
+# Get gramine dependencies
+sudo apt update
+sudo apt install build-essential autoconf bison gawk nasm -y
+sudo apt install ninja-build pkg-config python3 python3-click -y
+sudo apt install python3-jinja2 python3-pip python3-pyelftools wget -y
+sudo apt install meson python3-tomli python3-tomli-w -y
+sudo apt install libprotobuf-c-dev protobuf-c-compiler protobuf-compiler -y
+sudo apt install python3-cryptography python3-pip python3-protobuf -y
+
+# Download, build and install gramine-sgx
+cd $DEPS_DIR
+if [ -d "gramine" ]; then
+  echo "Gramine directory already exists -- skip cloning"
+else
+  echo "Cloning Gramine"
+  git clone https://github.com/gramineproject/gramine.git
+fi
+cd /$DEPS_DIR/gramine
+git checkout v1.5
+if [ -d "build-release" ]; then
+  echo "Gramine build-release directory already exists"
+  echo "Skipping Gramine build & install phase"
+else
+  echo "Building and installing Gramine in build-release directory"
+  meson setup build-release/ --buildtype=release -Dskeleton=enabled \
+  -Ddirect=enabled -Dsgx=enabled --prefix=$PWD/build-release
+  ninja -C build-release/
+  ninja -C build-release/ install
+fi
+
+# Download, build and install gramine-tdx
+cd $DEPS_DIR
+if [ -d "dkuvaisk.gramine-tdx" ]; then
+  echo "Gramine-TDX directory already exists -- skip cloning"
+else
+  echo "Cloning Gramine-TDX"
+  git clone https://github.com/intel-sandbox/dkuvaisk.gramine-tdx.git
+fi
+cd /$DEPS_DIR/dkuvaisk.gramine-tdx
+git checkout 78c2af00dca5eccdd190836c21c7065b11bf2c8b
+if [ -d "build-release" ]; then
+  echo "Gramine-TDX build-release directory already exists"
+  echo "Skipping Gramine-TDX build & install phase"
+else
+  echo "Building and installing Gramine-TDX in build-release directory"
+  meson setup build-release/ --buildtype=release -Dskeleton=enabled \
+  -Ddirect=enabled -Dsgx=enabled -Dvm=enabled -Dtdx=enabled \
+  --prefix=$PWD/build-release
+  ninja -C build-release/
+  ninja -C build-release/ install
+fi
+
+# Download the gramine-examples, their dependencies and apply the patch
+sudo apt install libnss-mdns libnss-myhostname -y
+sudo apt install python3-pip lsb-release -y
+pip3 install torchvision pillow
+cd $DEPS_DIR
+if [ -d "examples" ]; then
+  echo "Gramine examples directory already exists -- skip cloning and patching"
+else
+  echo "Cloning and patching Gramine examples"
+  git clone https://github.com/gramineproject/examples.git
+  cd $DEPS_DIR/examples
+  git checkout v1.5
+  git apply $EXAMPLES_PATCH
+fi
+echo "Building pytorch example"
+# Set-up paths for the gramine installation directory
+export PATH=$DEPS_DIR/gramine/build-release/bin:$PATH
+export PYTHONPATH=$DEPS_DIR/gramine/build-release/lib/python3.10/site-packages:$PYTHONPATH
+export PKG_CONFIG_PATH=$DEPS_DIR/gramine/build-release/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
+cd $DEPS_DIR/examples/pytorch
+if ! [ -f "$HOME/.config/gramine/enclave-key.pem" ]; then
+  echo "Generating SGX private key"
+  gramine-sgx-gen-private-key
+fi
+if ! [ -f "alexnet-pretrained.pt" ]; then
+  echo "Downloading the pre-trained model"
+  python3 download-pretrained-model.py
+fi
