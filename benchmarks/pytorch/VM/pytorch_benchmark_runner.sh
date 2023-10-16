a0268b3693e04e17fd7fc2fa7ed80eeae533e630
Dimitrios Stavrakakis <dimitrios.stavrakakis@intel.com>
2023-10-16 06:10:15 -0700

    [benchmarks/pytorch] Bind VMs to a single node

--- benchmarks/pytorch/VM/pytorch_benchmark_runner.sh
+++ benchmarks/pytorch/VM/pytorch_benchmark_runner.sh
@@ -5,8 +5,8 @@ set -e
 THIS_DIR=$(dirname "$(readlink -f "$0")")
 
 # Run the experiments
-vm_mem="16G" # VM memory
-epc_mem="8G" # EPC memory for the VM (only used for sgx)
+vm_mem="32G" # VM memory
+epc_mem="32G" # EPC memory for the VM (only used for sgx)
 vm_types=("td" "efi" "sgx")
 cpus=(1 2 4 6 8 12 16 20 24 32)
 
