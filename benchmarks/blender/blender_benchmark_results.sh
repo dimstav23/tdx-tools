7aa6e401692c86dad065549772c2f674529c426b
Dimitrios Stavrakakis <dimitrios.stavrakakis@intel.com>
2023-11-02 00:59:55 -0700

    [benchmarks/blender] rename the VM runner script and add the result parsing script
    Signed-off-by: Dimitrios Stavrakakis <dimitrios.stavrakakis@intel.com>

--- benchmarks/blender/blender_benchmark_results.sh
+++ benchmarks/blender/blender_benchmark_results.sh
@@ -15,3 +15,47 @@ sudo bash -c "cp -r $MOUNTPOINT/root/gramine/CI-Examples/blender/results/* $RESU
 
 sudo umount $MOUNTPOINT
 rm -rf $MOUNTPOINT
+
+# Pretty print the results
+declare -A data
+
+max_vm_type_length=0
+
+for file in $RESULTS_DIR/*.txt; do
+  file_name=$(basename "$file")
+  vm_type=$(echo "$file_name" | cut -d'_' -f1)
+  thread=$(echo "$file_name" | cut -d'_' -f2)
+  time_taken=$(cat "$file" | cut -d'(' -f1 | cut -d ':' -f3) # getting only the seconds
+  data["$vm_type,$thread"]=$time_taken
+
+  if [[ ${#vm_type} -gt max_vm_type_length ]]; then
+    max_vm_type_length=${#vm_type}
+  fi
+done
+
+vm_types=$(echo "${!data[@]}" | tr ' ' '\n' | cut -d',' -f1 | sort -u)
+thread_nums=$(echo "${!data[@]}" | tr ' ' '\n' | cut -d',' -f2 | sort -n -u)
+
+header="VM Type\Threads"
+if [[ ${#header} -gt max_vm_type_length ]]; then
+  max_vm_type_length=${#header}
+fi
+
+printf "%-${max_vm_type_length}s\t" "VM Type\Threads"
+for thread in $thread_nums; do
+  printf "%s\t" "$thread"
+done
+printf "\n"
+
+for vm_type in $vm_types; do
+  printf "%-${max_vm_type_length}s\t" "$vm_type"
+  for thread in $thread_nums; do
+    key="$vm_type,$thread"
+    if [ -n "${data[$key]}" ]; then
+      printf "%s\t" "${data[$key]}"
+    else
+      printf "N/A\t"
+    fi
+  done
+  printf "\n"
+done
