a5945845b180726b22725e0c06f0604ba5cff61d
Dimitrios Stavrakakis <dimitrios.stavrakakis@intel.com>
2023-11-08 13:56:05 -0800

    [benchmarks/python] add python automation scripts
    Signed-off-by: Dimitrios Stavrakakis <dimitrios.stavrakakis@intel.com>

---
+++ benchmarks/python/VM/VM_python_benchmark_runner.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+set -e
+
+THIS_DIR=$(dirname "$(readlink -f "$0")")
+
+# Run the experiments
+vm_mem="32G" # VM memory
+epc_mem="32G" # EPC memory for the VM (only used for sgx)
+vm_types=("td" "efi" "gramine-direct" "gramine-sgx")
+cpus=(1 2 4 6 8 12 16 20 24 32)
+
+for vm in "${vm_types[@]}"; do
+  for cpu in "${cpus[@]}"; do
+    $THIS_DIR/VM_python_benchmark.expect $vm $cpu $vm_mem $epc_mem
+  done
+done
---
+++ benchmarks/python/bare-metal/bare-metal_python_benchmark_runner.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/bash
+
+set -e
+
+THIS_DIR=$(dirname "$(readlink -f "$0")")
+DEPS_DIR=$THIS_DIR/../../common/bare-metal/deps
+RESULTS_DIR=$THIS_DIR/../results
+GRAMINE_SGX_INSTALL_DIR=$DEPS_DIR/gramine/build-release
+GRAMINE_TDX_INSTALL_DIR=$DEPS_DIR/dkuvaisk.gramine-tdx/build-release
+BENCHMARK_DIR=$DEPS_DIR/gramine/CI-Examples/python
+
+THREADS=(1 2 4 6 8 12 16 20 24 32)
+
+pushd $BENCHMARK_DIR
+
+#create temp directory for the results
+mkdir -p results
+
+# Run the native case
+for THREAD_CNT in "${THREADS[@]}"; do
+  export OMP_NUM_THREADS=$THREAD_CNT
+  export OPENBLAS_NUM_THREADS=$THREAD_CNT
+  numactl --cpunodebind=0 --membind=0 python scripts/test-numpy.py \
+  | tail -n 2 | tee ./results/numpy_native_"$THREAD_CNT"_threads.txt
+  numactl --cpunodebind=0 --membind=0 python scripts/test-scipy.py \
+  | tail -n 4 | tee ./results/scipy_native_"$THREAD_CNT"_threads.txt
+done
+
+# Preserve the current values of the env variables
+CURR_PATH=$PATH
+CURR_PYTHONPATH=$PYTHONPATH
+CURR_PKG_CONFIG_PATH=$PKG_CONFIG_PATH
+
+# Run the bare-metal (bm) gramine-direct and gramine-sgx case
+export PATH=$GRAMINE_SGX_INSTALL_DIR/bin:$CURR_PATH
+export PYTHONPATH=$GRAMINE_SGX_INSTALL_DIR/lib/python3.10/site-packages:$CURR_PYTHONPATH
+export PKG_CONFIG_PATH=$GRAMINE_SGX_INSTALL_DIR/lib/x86_64-linux-gnu/pkgconfig:$CURR_PKG_CONFIG_PATH
+make clean && make SGX=1
+for THREAD_CNT in "${THREADS[@]}"; do
+  export OMP_NUM_THREADS=$THREAD_CNT
+  export OPENBLAS_NUM_THREADS=$THREAD_CNT
+  numactl --cpunodebind=0 --membind=0 gramine-direct python scripts/test-numpy.py \
+  | tail -n 2 | tee ./results/numpy_bm-gramine-direct_"$THREAD_CNT"_threads.txt
+  numactl --cpunodebind=0 --membind=0 gramine-direct python scripts/test-scipy.py \
+  | tail -n 4 | tee ./results/scipy_bm-gramine-direct_"$THREAD_CNT"_threads.txt
+done
+for THREAD_CNT in "${THREADS[@]}"; do
+  export OMP_NUM_THREADS=$THREAD_CNT
+  export OPENBLAS_NUM_THREADS=$THREAD_CNT
+  numactl --cpunodebind=0 --membind=0 gramine-sgx python scripts/test-numpy.py \
+  | tail -n 2 | tee ./results/numpy_bm-gramine-sgx_"$THREAD_CNT"_threads.txt
+  numactl --cpunodebind=0 --membind=0 gramine-sgx python scripts/test-scipy.py \
+  | tail -n 4 | tee ./results/scipy_bm-gramine-sgx_"$THREAD_CNT"_threads.txt
+done
+
+# Run the gramine-vm and gramine-tdx case
+export PATH=$GRAMINE_TDX_INSTALL_DIR/bin:$CURR_PATH
+export PYTHONPATH=$GRAMINE_TDX_INSTALL_DIR/lib/python3.10/site-packages:$CURR_PYTHONPATH
+export PKG_CONFIG_PATH=$GRAMINE_TDX_INSTALL_DIR/lib/x86_64-linux-gnu/pkgconfig:$CURR_PKG_CONFIG_PATH
+make clean && make SGX=1
+for THREAD_CNT in "${THREADS[@]}"; do
+  export QEMU_CPU_NUM=$THREAD_CNT
+  export OMP_NUM_THREADS=$THREAD_CNT
+  export OPENBLAS_NUM_THREADS=$THREAD_CNT
+  numactl --cpunodebind=0 --membind=0 gramine-vm python scripts/test-numpy.py \
+  | tail -n 2 | tee ./results/numpy_bm-gramine-vm_"$THREAD_CNT"_threads.txt
+  numactl --cpunodebind=0 --membind=0 gramine-vm python scripts/test-scipy.py \
+  | tail -n 4 | tee ./results/scipy_bm-gramine-vm_"$THREAD_CNT"_threads.txt
+done
+for THREAD_CNT in "${THREADS[@]}"; do
+  export QEMU_CPU_NUM=$THREAD_CNT
+  export OMP_NUM_THREADS=$THREAD_CNT
+  export OPENBLAS_NUM_THREADS=$THREAD_CNT
+  numactl --cpunodebind=0 --membind=0 gramine-tdx python scripts/test-numpy.py \
+  | tail -n 2 | tee ./results/numpy_bm-gramine-tdx_"$THREAD_CNT"_threads.txt
+  numactl --cpunodebind=0 --membind=0 gramine-tdx python scripts/test-scipy.py \
+  | tail -n 4 | tee ./results/scipy_bm-gramine-tdx_"$THREAD_CNT"_threads.txt
+done
+
+mkdir -p $RESULTS_DIR
+mv results/* $RESULTS_DIR
+rm -rf results
+
+popd
