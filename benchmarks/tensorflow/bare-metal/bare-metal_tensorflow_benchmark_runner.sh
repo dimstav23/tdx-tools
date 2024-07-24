8412eeefce2f8d3950702dac428a728c046798b5
dimstav23 <dimstav23@gmail.com>
2024-07-24 14:07:44 -0700

    [benchmarks/tensorflow] fix tensorflow run
    Signed-off-by: dimstav23 <dimstav23@gmail.com>

--- benchmarks/tensorflow/bare-metal/bare-metal_tensorflow_benchmark_runner.sh
+++ benchmarks/tensorflow/bare-metal/bare-metal_tensorflow_benchmark_runner.sh
@@ -60,7 +60,7 @@ CURR_PKG_CONFIG_PATH=$PKG_CONFIG_PATH
 
 # Run the bare-metal (bm) gramine-sgx case
 export PATH=$GRAMINE_SGX_INSTALL_DIR/bin:$CURR_PATH
-export PYTHONPATH=$GRAMINE_SGX_INSTALL_DIR/lib/python3.10/site-packages:$CURR_PYTHONPATH
+export PYTHONPATH=$GRAMINE_SGX_INSTALL_DIR/lib/$(python3 -c 'import sys; print(f"python{sys.version_info.major}.{sys.version_info.minor}")')/site-packages:$CURR_PYTHONPATH
 export PKG_CONFIG_PATH=$GRAMINE_SGX_INSTALL_DIR/lib/x86_64-linux-gnu/pkgconfig:$CURR_PKG_CONFIG_PATH
 make clean && make SGX=1
 for THREAD_CNT in "${THREADS[@]}"; do
@@ -100,7 +100,7 @@ done
 
 # Run the gramine-vm and gramine-tdx case
 export PATH=$GRAMINE_TDX_INSTALL_DIR/bin:$CURR_PATH
-export PYTHONPATH=$GRAMINE_TDX_INSTALL_DIR/lib/python3.10/site-packages:$CURR_PYTHONPATH
+export PYTHONPATH=$GRAMINE_TDX_INSTALL_DIR/lib/$(python3 -c 'import sys; print(f"python{sys.version_info.major}.{sys.version_info.minor}")')/site-packages:$CURR_PYTHONPATH
 export PKG_CONFIG_PATH=$GRAMINE_TDX_INSTALL_DIR/lib/x86_64-linux-gnu/pkgconfig:$CURR_PKG_CONFIG_PATH
 make clean && make SGX=1
 for THREAD_CNT in "${THREADS[@]}"; do
--- benchmarks/tensorflow/tensorflow_benchmark_results.sh
+++ benchmarks/tensorflow/tensorflow_benchmark_results.sh
@@ -9,7 +9,7 @@ RESULTS_DIR=$THIS_DIR/results
 
 # Gather the results from the guest image
 mkdir -p $MOUNTPOINT
-sudo guestmount -a $THIS_DIR/../common/VM/td-guest-ubuntu-22.04.qcow2 -i --ro $MOUNTPOINT
+sudo guestmount -a $THIS_DIR/../common/VM/tdx/guest-tools/image/tdx-guest-ubuntu-24.04-generic.qcow2 -i --ro $MOUNTPOINT
 
 mkdir -p $RESULTS_DIR
 sudo bash -c "cp -r $VM_RESULT_DIR/* $RESULTS_DIR 2>/dev/null || true"
