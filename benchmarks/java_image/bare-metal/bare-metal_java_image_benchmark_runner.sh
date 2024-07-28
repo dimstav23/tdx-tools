c24eff95153a7d618a4034c417f9f72b242425b4
dimstav23 <dimstav23@gmail.com>
2024-07-28 11:42:55 -0700

    [benchmarks/java_image] add the automation for the java image processing  example
    Signed-off-by: dimstav23 <dimstav23@gmail.com>

---
+++ benchmarks/java_image/bare-metal/bare-metal_java_image_benchmark_runner.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/bash
+
+set -e
+
+THIS_DIR=$(dirname "$(readlink -f "$0")")
+DEPS_DIR=$THIS_DIR/../../common/bare-metal/deps
+RESULTS_DIR=$THIS_DIR/../results
+GRAMINE_SGX_INSTALL_DIR=$DEPS_DIR/gramine/build-release
+GRAMINE_TDX_INSTALL_DIR=$DEPS_DIR/gramine-tdx/build-release
+BENCHMARK_DIR=$DEPS_DIR/examples/java_image
+
+THREADS=(1 2 4 8 16 32)
+
+pushd $BENCHMARK_DIR
+
+#create temp directory for the results
+mkdir -p results
+
+# make might require gramine binaries, so just export the paths
+CURR_PATH=$PATH
+CURR_PYTHONPATH=$PYTHONPATH
+CURR_PKG_CONFIG_PATH=$PKG_CONFIG_PATH
+export PATH=$GRAMINE_TDX_INSTALL_DIR/bin:$CURR_PATH
+export PYTHONPATH=$GRAMINE_TDX_INSTALL_DIR/lib/$(python3 -c 'import sys; print(f"python{sys.version_info.major}.{sys.version_info.minor}")')/site-packages:$CURR_PYTHONPATH
+export PKG_CONFIG_PATH=$GRAMINE_TDX_INSTALL_DIR/lib/x86_64-linux-gnu/pkgconfig:$CURR_PKG_CONFIG_PATH
+make clean
+make SGX=1
+
+# Run the native case
+for THREAD_CNT in "${THREADS[@]}"; do
+  # Manual candle execution, get the last 2 lines for the results with the metrics (tokens/s)
+  NUM_THREADS=$THREAD_CNT numactl --cpunodebind=0 --membind=0 java ImageProcessing input.png \
+  | tail -n 3 | tee ./results/native_"$THREAD_CNT"_threads.txt
+done
+
+# Run the bare-metal (bm) gramine-sgx case
+export PATH=$GRAMINE_SGX_INSTALL_DIR/bin:$CURR_PATH
+export PYTHONPATH=$GRAMINE_SGX_INSTALL_DIR/lib/$(python3 -c 'import sys; print(f"python{sys.version_info.major}.{sys.version_info.minor}")')/site-packages:$CURR_PYTHONPATH
+export PKG_CONFIG_PATH=$GRAMINE_SGX_INSTALL_DIR/lib/x86_64-linux-gnu/pkgconfig:$CURR_PKG_CONFIG_PATH
+make clean
+make SGX=1
+for THREAD_CNT in "${THREADS[@]}"; do
+  NUM_THREADS=$THREAD_CNT numactl --cpunodebind=0 --membind=0 gramine-sgx java ImageProcessing input.png \
+  | tail -n 3 | tee ./results/bm-gramine-sgx_"$THREAD_CNT"_threads.txt
+done
+
+# Run the gramine-vm and gramine-tdx case
+export PATH=$GRAMINE_TDX_INSTALL_DIR/bin:$CURR_PATH
+export PYTHONPATH=$GRAMINE_TDX_INSTALL_DIR/lib/$(python3 -c 'import sys; print(f"python{sys.version_info.major}.{sys.version_info.minor}")')/site-packages:$CURR_PYTHONPATH
+export PKG_CONFIG_PATH=$GRAMINE_TDX_INSTALL_DIR/lib/x86_64-linux-gnu/pkgconfig:$CURR_PKG_CONFIG_PATH
+make clean
+make SGX=1
+for THREAD_CNT in "${THREADS[@]}"; do
+  NUM_THREADS=$THREAD_CNT numactl --cpunodebind=0 --membind=0 gramine-vm java ImageProcessing input.png \
+  | tail -n 3 | tee ./results/gramine-vm_"$THREAD_CNT"_threads.txt
+done
+for THREAD_CNT in "${THREADS[@]}"; do
+  NUM_THREADS=$THREAD_CNT numactl --cpunodebind=0 --membind=0 gramine-tdx java ImageProcessing input.png \
+  | tail -n 3 | tee ./results/gramine-tdx_"$THREAD_CNT"_threads.txt
+done
+
+mkdir -p $RESULTS_DIR
+mv results/* $RESULTS_DIR
+rm -rf results
+
+popd
---
+++ benchmarks/java_image/java_image_benchmark_results.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/bash
+
+set -e
+
+THIS_DIR=$(dirname "$(readlink -f "$0")")
+MOUNTPOINT=$THIS_DIR/tmp_mnt
+VM_RESULT_DIR=$MOUNTPOINT/root/examples/java_image/results
+RESULTS_DIR=$THIS_DIR/results
+
+# Gather the results from the guest image
+mkdir -p $MOUNTPOINT
+sudo guestmount -a $THIS_DIR/../common/VM/tdx/guest-tools/image/tdx-guest-ubuntu-24.04-generic.qcow2 -i --ro $MOUNTPOINT
+
+mkdir -p $RESULTS_DIR
+sudo bash -c "cp -r $VM_RESULT_DIR/* $RESULTS_DIR 2>/dev/null || true"
+
+sudo umount $MOUNTPOINT
+rm -rf $MOUNTPOINT
+
+# Organize the results
+declare -A data
+
+max_vm_type_length=0
+
+for file in $RESULTS_DIR/*.txt; do
+  file_name=$(basename "$file")
+  vm_type=$(echo "$file_name" | cut -d'_' -f1)
+  thread=$(echo "$file_name" | cut -d'_' -f2)
+  time_taken=$(cat "$file" | grep "Time taken:" | cut -d' ' -f3) # getting only the milliseconds
+  time_taken_s=$(echo "scale=3; $time_taken / 1000" | bc) # convert to S
+  data["$vm_type,$thread"]=$time_taken_s
+
+  if [[ ${#vm_type} -gt max_vm_type_length ]]; then
+    max_vm_type_length=${#vm_type}
+  fi
+done
+
+vm_types=$(echo "${!data[@]}" | tr ' ' '\n' | cut -d',' -f1 | sort -u)
+thread_nums=$(echo "${!data[@]}" | tr ' ' '\n' | cut -d',' -f2 | sort -n -u)
+
+# Output the results to csv
+for vm_type in $vm_types; do
+  csv_file="$RESULTS_DIR/"$vm_type".csv"
+  # write the header of the csv
+  printf "Threads," > $csv_file
+  for thread in $thread_nums; do
+    printf "%s," "$thread" >> $csv_file
+  done
+  truncate -s-1 $csv_file # remove last comma
+  printf "\n" >> $csv_file
+
+  printf "Runtime (sec)," >> $csv_file
+  for thread in $thread_nums; do
+    key="$vm_type,$thread"
+    if [ -n "${data[$key]}" ]; then
+      printf "%.4f," "${data[$key]}" >> $csv_file
+    else
+      printf "," >> $csv_file
+    fi
+  done
+  truncate -s-1 $csv_file # remove last comma
+  printf "\n" >> $csv_file
+done
+
+# Pretty print the results to stdout
+header="VM Type\Threads"
+if [[ ${#header} -gt max_vm_type_length ]]; then
+  max_vm_type_length=${#header}
+fi
+
+printf "%-${max_vm_type_length}s\t" "$header"
+for thread in $thread_nums; do
+  printf "%s\t" "$thread"
+done
+printf "\n"
+
+for vm_type in $vm_types; do
+  printf "%-${max_vm_type_length}s\t" "$vm_type"
+  for thread in $thread_nums; do
+    key="$vm_type,$thread"
+    if [ -n "${data[$key]}" ]; then
+      printf "%.4f\t" "${data[$key]}"
+    else
+      printf "N/A\t"
+    fi
+  done
+  printf "\n"
+done
