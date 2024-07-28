7957ad207fc1f3f425217d403e9d68b9e684b972
dimstav23 <dimstav23@gmail.com>
2024-07-28 11:41:50 -0700

    [benchmarks/bild] add the automation for the go example based on the bild framework
    Signed-off-by: dimstav23 <dimstav23@gmail.com>

---
+++ benchmarks/bild/automated_runner.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+
+set -e
+
+THIS_DIR=$(dirname "$(readlink -f "$0")")
+
+cd $THIS_DIR/bare-metal
+./bare-metal_bild_benchmark_runner.sh
+
+cd $THIS_DIR/VM
+./VM_bild_benchmark_runner.sh
+
+cd $THIS_DIR
+./bild_benchmark_results.sh
+
+mv results results_gramine_tdx_1_6_1
+
+cd $THIS_DIR/bare-metal
+./bare-metal_bild_benchmark_runner.sh
+
+cd $THIS_DIR/VM
+./VM_bild_benchmark_runner.sh
+
+cd $THIS_DIR
+./bild_benchmark_results.sh
+
+mv results results_gramine_tdx_1_6_2
+
+cd $THIS_DIR/bare-metal
+./bare-metal_bild_benchmark_runner.sh
+
+cd $THIS_DIR/VM
+./VM_bild_benchmark_runner.sh
+
+cd $THIS_DIR
+./bild_benchmark_results.sh
+
+mv results results_gramine_tdx_1_6_3
