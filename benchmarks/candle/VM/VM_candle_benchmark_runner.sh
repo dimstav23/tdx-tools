8a83341c43b798e43474430dc5509cb2cfc1ca9f
dimstav23 <dimstav23@gmail.com>
2024-07-28 11:44:20 -0700

    [benchmarks/candle] add the automation for the candle example
    Signed-off-by: dimstav23 <dimstav23@gmail.com>

---
+++ benchmarks/candle/VM/VM_candle_benchmark_runner.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+set -e
+
+THIS_DIR=$(dirname "$(readlink -f "$0")")
+
+# Run the experiments
+vm_mem="32G" # VM memory
+vm_types=("td" "efi")
+cpus=(1 2 4 8 16 32)
+
+for vm in "${vm_types[@]}"; do
+  for cpu in "${cpus[@]}"; do
+    $THIS_DIR/VM_candle_benchmark.expect $vm $cpu $vm_mem
+  done
+done
